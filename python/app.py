f0ef54bcc9213101b30a3bfdea31fe87ae8ef786
srinivasareddy-pidaparti <90228410+srinivasareddy-pidaparti@users.noreply.github.com>
2022-11-28 22:17:57 -0600

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! srinivas"
+    return "Hello World! srinivas pidaparti"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
